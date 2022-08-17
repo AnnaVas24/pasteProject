58fe2fad14af27ada53ff55f179753c1cd2aa00f
Anna <anna.vasi4ko@icloud.com>
2022-08-17 19:48:27 +0300

    paste control

--- pasteProject/ViewController.swift
+++ pasteProject/ViewController.swift
@@ -8,12 +8,68 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mainTextView: UITextView!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    
+   // let pasteController = UIPasteControl()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupMenu()
+        setupTextField()
+
+//      textField.inputAccessoryView = pasteController
+//      pasteController.target = textField - так не работает
     }
-
-
+    
+    
+    private func setupTextField() {
+        let pasteController = UIPasteControl()
+//        let configuration = UIPasteControl.Configuration()
+//        configuration.displayMode = .iconOnly
+//        pasteController.configuration = configuration - так тоже нельзя поменять - это get свойство
+//        pasteController.configuration.displayMode = .iconOnly  //почему-то таким образом не дает менять конфигурацию
+        
+        pasteController.target = textField
+        
+        textField.rightView = pasteController
+        textField.rightViewMode = .always
+  
+    }
+    private func setupMenu() {
+        let menuItems: [UIAction] = [
+            UIAction(
+                title: "Copy",
+                image: UIImage(systemName: "doc.on.doc"),
+                handler: { _ in
+                    
+                }
+            ),
+            UIAction(
+                title: "Delete",
+                image: UIImage(systemName: "trash"),
+                handler: { _ in
+                    
+                }
+            ),
+            UIAction(
+                identifier:UIAction.Identifier.paste,
+                handler: { _ in
+                    let pasteboardContent = UIPasteboard.general.string
+                    if let pasteboardContent {
+                        self.mainTextView.text = pasteboardContent
+                    }
+                }
+            )
+        ]
+        
+        let demoMenu = UIMenu(children: menuItems)
+        
+        let plusButton = UIBarButtonItem(image: UIImage(systemName: "plus.circle"), menu: demoMenu)
+        navigationItem.rightBarButtonItem = plusButton
+    }
 }
 
